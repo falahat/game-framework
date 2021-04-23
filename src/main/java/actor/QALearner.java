7413ee44b1d5347482c1785222f79662cff6ad9e
Aryan Falahatpisheh <aryanfalahat@gmail.com>
2021-04-22 18:35:19 -0700

    Delegate learning/deciding so that we can use different reinforcement learning algorithms without having to refactor SmartPerson.java

---
+++ src/main/java/actor/QALearner.java
@@ -0,0 +1,70 @@
+package actor;
+
+import state.GameStateView;
+import state.RelativePositionedView;
+import state.board.ReadableBoard;
+import state.board.WritableBoard;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class QALearner implements Trainable<ReadableBoard, WritableBoard> {
+    public static final double DEFAULT_SCORE = 50;
+    public static final double LEARNING_RATE = 0.12;
+    public static final double DISCOUNT_RATE = 0.5;
+    public static final boolean ALWAYS_PICK_BEST_ACTION = false;
+
+    private final Map<RelativePositionedView, Map<String, Double>> valueOfActionPerState;
+
+    public QALearner() {
+        this.valueOfActionPerState = new HashMap<>();
+    }
+
+    @Override
+    public Action<ReadableBoard, WritableBoard> decide(GameStateView currentState, Collection<Action<ReadableBoard, WritableBoard>> allowedActions) {
+        Random random = new Random();
+
+        List<Action<ReadableBoard, WritableBoard>> sorted = allowedActions.stream()
+                .sorted(Comparator.comparing(action -> random.nextDouble()-this.getEstimatedScore((RelativePositionedView) currentState, action)))
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < sorted.size()-1; i++) {
+            if (ALWAYS_PICK_BEST_ACTION || random.nextBoolean() || random.nextBoolean() || random.nextBoolean()) {
+                return sorted.get(i);
+            }
+        }
+
+        return sorted.get(sorted.size()-1);
+    }
+
+    @Override
+    public void learn(Action<ReadableBoard, WritableBoard> decided,
+                      GameStateView firstState,
+                      GameStateView nextState, double immediateReward) {
+        // we were at "firstState" and chose "decided".
+        double originalEstimate = getEstimatedScore((RelativePositionedView) firstState, decided);
+
+        double maxValueFromNewState = getMaximumEstimateScore((RelativePositionedView) nextState);
+
+        double estimateDiff = (immediateReward + DISCOUNT_RATE * maxValueFromNewState) - originalEstimate;
+        double newEstimate = originalEstimate + LEARNING_RATE * estimateDiff;
+
+        this.valueOfActionPerState.putIfAbsent((RelativePositionedView) firstState, new HashMap<>());
+        this.valueOfActionPerState.get(firstState).put(decided.uniqueActionType(), newEstimate);
+    }
+
+
+    public double getEstimatedScore(RelativePositionedView state, Action chosen) {
+        String actionKey = chosen.uniqueActionType();
+        return valueOfActionPerState
+                .getOrDefault(state, Collections.emptyMap())
+                .getOrDefault(actionKey, DEFAULT_SCORE);
+    }
+
+    public double getMaximumEstimateScore(RelativePositionedView state) {
+        return valueOfActionPerState
+                .getOrDefault(state, Collections.emptyMap()).values().stream().mapToDouble(a -> a)
+                .max()
+                .orElse(DEFAULT_SCORE);
+    }
+}
--- src/main/java/actor/SmartPerson.java
+++ src/main/java/actor/SmartPerson.java
@@ -10,64 +10,32 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class SmartPerson extends Person {
-    public static final double DEFAULT_SCORE = 50;
-    public static final double LEARNING_RATE = 0.12;
-    public static final double DISCOUNT_RATE = 0.5;
-    public static final boolean ALWAYS_PICK_BEST_ACTION = false;
 
-    private final Map<RelativePositionedView, Map<String, Double>> valueOfActionPerState;
+
+    private final QALearner brain;
 
     public SmartPerson(Direction direction) {
         super(direction);
-        this.valueOfActionPerState = new HashMap<>();
+        this.brain = new QALearner();
     }
 
     @Override
     public Action<ReadableBoard, WritableBoard> decide(GameStateView currentState, Collection<Action<ReadableBoard, WritableBoard>> allowedActions) {
-        Random random = new Random();
-
-        List<Action<ReadableBoard, WritableBoard>> sorted = allowedActions.stream()
-                .sorted(Comparator.comparing(action -> random.nextDouble()-this.getEstimatedScore((RelativePositionedView) currentState, action)))
-                .collect(Collectors.toList());
-
-        for (int i = 0; i < sorted.size()-1; i++) {
-            if (ALWAYS_PICK_BEST_ACTION || random.nextBoolean() || random.nextBoolean() || random.nextBoolean()) {
-                return sorted.get(i);
-            }
-        }
-
-        return sorted.get(sorted.size()-1);
+        return brain.decide(currentState, allowedActions);
     }
 
     @Override
     public void learn(Action<ReadableBoard, WritableBoard> decided,
                       GameStateView firstState,
                       GameStateView nextState, double immediateReward) {
-        super.learn(decided, firstState, nextState, immediateReward);
-
-        // we were at "firstState" and chose "decided".
-        double originalEstimate = getEstimatedScore((RelativePositionedView) firstState, decided);
-
-        double maxValueFromNewState = getMaximumEstimateScore((RelativePositionedView) nextState);
-
-        double estimateDiff = (immediateReward + DISCOUNT_RATE * maxValueFromNewState) - originalEstimate;
-        double newEstimate = originalEstimate + LEARNING_RATE * estimateDiff;
-
-        this.valueOfActionPerState.putIfAbsent((RelativePositionedView) firstState, new HashMap<>());
-        this.valueOfActionPerState.get(firstState).put(decided.uniqueActionType(), newEstimate);
+        brain.learn(decided, firstState, nextState, immediateReward);
     }
 
     public double getEstimatedScore(RelativePositionedView state, Action chosen) {
-        String actionKey = chosen.uniqueActionType();
-        return valueOfActionPerState
-                .getOrDefault(state, Collections.emptyMap())
-                .getOrDefault(actionKey, DEFAULT_SCORE);
+        return brain.getEstimatedScore(state, chosen);
     }
 
     public double getMaximumEstimateScore(RelativePositionedView state) {
-        return valueOfActionPerState
-                .getOrDefault(state, Collections.emptyMap()).values().stream().mapToDouble(a -> a)
-                .max()
-                .orElse(DEFAULT_SCORE);
+        return brain.getMaximumEstimateScore(state);
     }
 }
